1eb73a37d7fe5fc8ebc1446b309ff594942d36a3
ashawn900311 <ashawn900311@gmail.com>
2026-06-30 22:25:18 +0800

    Create LeetCode7.cpp

---
+++ AssignmentII/Before-Class_Exercises/Week1_01_LeetCode7/src/LeetCode7.cpp
@@ -0,0 +1,20 @@
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        long long result = 0;
+
+        while (x != 0) {
+            result = result * 10 + x % 10;
+            x /= 10;
+
+            if (result > INT_MAX || result < INT_MIN) {
+                return 0;
+            }
+        }
+
+        return static_cast<int>(result);
+    }
+};
