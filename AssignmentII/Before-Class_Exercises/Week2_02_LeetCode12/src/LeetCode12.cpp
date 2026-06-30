a475c121673ac8d05742a5df20f448934c77e73f
ashawn900311 <ashawn900311@gmail.com>
2026-06-30 22:26:21 +0800

    Create LeetCode12.cpp

---
+++ AssignmentII/Before-Class_Exercises/Week2_02_LeetCode12/src/LeetCode12.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<pair<int, string>> values = {
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"},
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
+            {40, "XL"},
+            {10, "X"},
+            {9, "IX"},
+            {5, "V"},
+            {4, "IV"},
+            {1, "I"}
+        };
+
+        string result;
+
+        for (auto item : values) {
+            while (num >= item.first) {
+                result += item.second;
+                num -= item.first;
+            }
+        }
+
+        return result;
+    }
+};
